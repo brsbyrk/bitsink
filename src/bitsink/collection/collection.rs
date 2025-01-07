52773f12aee4ac37105f5a8679cce6a87be2fb2d
Baris <baris@bayrak.me>
2025-01-07 18:59:13 +0300

    clean up

--- src/bitsink/collection/collection.rs
+++ src/bitsink/collection/collection.rs
@@ -9,23 +9,3 @@ pub struct Collection {
     description: String,
     fields: Vec<Field>,
 }
-
-
-
-
-
-
-// CREATE TABLE collections (
-//     id TEXT PRIMARY KEY,
-//     project_id TEXT NOT NULL,
-//     name TEXT NOT NULL,
-//     FOREIGN KEY (project_id) REFERENCES projects (id)
-// );
-
-// CREATE TABLE fields (
-//     id TEXT PRIMARY KEY,
-//     collection_id TEXT NOT NULL,
-//     name TEXT NOT NULL,
-//     type TEXT NOT NULL,
-//     FOREIGN KEY (collection_id) REFERENCES collections (id)
-// );
